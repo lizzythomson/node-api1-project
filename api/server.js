aa0361c4d31918e4b10ab8c93cf67db72cfae8ca
Lizzy Thomson <71537606+lizzythomson@users.noreply.github.com>
2022-02-07 13:50:34 -0700

    added appropriate dependencies and endpoint for POSTing a new user

--- api/server.js
+++ api/server.js
@@ -1,3 +1,54 @@
 // BUILD YOUR SERVER HERE
 
-module.exports = {}; // EXPORT YOUR SERVER instead of {}
+const express = require('express');
+const userModel = require('./users/model');
+
+const server = express();
+
+server.use(express.json());
+
+server.post('/api/users', (req, res) => {
+  const body = req.body;
+  if (!body.name) {
+    res
+      .status(400)
+      .json({ message: 'Please provide name and bio for the user' });
+  } else if (!body.bio) {
+    res
+      .status(400)
+      .json({ message: 'Please provide name and bio for the user' });
+  } else {
+    userModel
+      .create(body)
+      .then((user) => {
+        res.status(201).json(user);
+      })
+      .catch(() => {
+        res.status(500).json({
+          message: 'There was an error while saving the user to the database',
+        });
+      });
+  }
+});
+
+server.get('/api/users', (req, res) => {
+  userModel
+    .find()
+    .then((users) => {
+      console.log(users);
+      res.json(users);
+    })
+    .catch(() => {
+      res
+        .status(500)
+        .json({ message: 'The users information could not be retrieved' });
+    });
+});
+
+// POST	/api/users
+// GET	/api/users
+// GET	/api/users/:id
+// DELETE	/api/users/:id
+// PUT	/api/users/:id
+
+module.exports = server; // EXPORT YOUR SERVER instead of {}
